2eca90d4443ee7c8ec0c8e64c7af1a3632dde861
chiiriill <98037071+chiiriill@users.noreply.github.com>
2022-05-14 21:48:39 +0300

    final draft without readme

--- scraping.py
+++ scraping.py
@@ -1,44 +1,67 @@
+import random
 import time
-
 import requests
 from bs4 import BeautifulSoup
 import csv
 import os
-import datetime
+from art import *
+from fake_useragent import UserAgent
+
+HEADERS = {
+    'user-agent': UserAgent().random,
+    'accept': '*/*'
+}
+
+LANGUAGES = {
+    'EN': {
+        'start_program': 'Welcome to my program!'
+                         ' She can take the site menu and write it to a csv file.\nThere are two ways to write a menu:'
+                         ' the entire menu at once into one file, or each category into a separate file.',
+        'change_language': 'Do you want to change language?[+/-]: ',
+        'language_was_change': 'The language has been changed to',
+        'parser_choice': 'Choose how you want to take the menu from the sushihouse website: 1 if the entire menu is'
+                         ' written to one file, 2 if each category is written to a separate file: ',
+        'unknown_parser': 'Unknown choice! Try again :)',
+        'start_process': 'Start of process:',
+        'process_finished': 'The process is over! The file has been generated :)',
+        'info_about_process': 'Information about the work of the program:'
+    },
+    'RU': {
+        'start_program': 'Добро пожаловать в мою программу!'
+                         ' Она умеет брать меню сайта и записывать его в csv файл.\nЕсть два способа'
+                         ' записи меню: сразу всё меню в один файл или каждая категория в отдельный файл. ',
+        'change_language': 'Вы хотите сменить язык?[+/-]: ',
+        'language_was_change': 'Язык был изменен на',
+        'parser_choice': 'Выберите как вы хотите взять меню с сайта сушидома: 1 если всё меню записать в один файл, '
+                         '2 если каждая категория записывается в отдельный файл: ',
+        'unknown_parser': 'Неизвестный выбор! Попробуйте еще раз :)',
+        'start_process': 'Начало процесса:',
+        'process_finished': 'Процесс закончен! Файл сформирован :)',
+        'info_about_process': 'Информация о работе программы:'
+
+    }
+}
 
 
-def all_menu(url, headers):
-    site = requests.get(url=url, headers=headers)
-    soup = BeautifulSoup(site.text, 'lxml')
-    amount_pages = int(soup.find_all('a', class_='page-numbers')[-2].text)
-
+def create_dir(type_parser):
     if not os.path.exists('data'):
         os.mkdir('data')
 
-    if not os.path.exists('data/all_menu'):
-        os.mkdir('data/all_menu')
+    if not os.path.exists(f'data/{type_parser}'):
+        os.mkdir(f'data/{type_parser}')
 
-    with open('data/all_menu/all_menu.csv', 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow(
-            (
-                'Название',
-                'Состав',
-                'Цена',
-                'Ссылка на товар'
-            )
-        )
 
+def update_csv_file_new_dishes(url, amount_pages, type_parser, title):
     for page in range(1, amount_pages + 1):
-        url = f'https://sushidom.by/shop/page/{page}'
-        site = requests.get(url=url, headers=headers)
+        url = f'{url}page/{page}'
+        site = requests.get(url=url, headers=HEADERS)
         soup = BeautifulSoup(site.text, 'lxml')
         products = soup.find_all('div', class_='shop-cards')
         for product in products:
             try:
-                title = product.find('h5').text
+                name = product.find('h5').text
             except Exception:
-                title = 'Нет названия'
+                name = 'Нет названия'
 
             try:
                 url = product.find('h5').find('a').get('href')
@@ -63,31 +86,112 @@ def all_menu(url, headers):
             except Exception:
                 price = 'Всё зависит от количества'
 
-            with open('data/all_menu/all_menu.csv', 'a') as file:
+            with open(f'data/{type_parser}/{title}.csv', 'a') as file:
                 writer = csv.writer(file)
                 writer.writerow(
                     (
-                        title.strip(),
+                        name.strip(),
                         compounds.strip(),
                         price.strip(),
                         url.strip(),
                     )
                 )
-
+        time.sleep(random.randint(2, 4))
         print(f'\033[32m[+] page {page}/{amount_pages} DONE!')
 
 
-def main():
-    start_time = time.time()
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
-        'accept': '*/*'
-    }
-    url = 'https://sushidom.by/shop/'
-    all_menu(url, headers)
-    end_time = time.time()
-    print(f'[INFO] TIME WORK PROGRAMM: {(end_time - start_time):.2f} SECONDS')
+def info(func):
+    def inner(*args, **kwargs):
+        print(f'\033[35m{LANGUAGES[lang]["start_process"]}')
+        start_time = time.time()
+        func(*args, **kwargs)
+        end_time = time.time()
+        print(f'\033[35m{LANGUAGES[lang]["process_finished"]}')
+        print(f'\033[35m{LANGUAGES[lang]["info_about_process"]}')
+        print(f'\033[35mTIME WORK PROGRAM: {(end_time - start_time):.2f} SECONDS')
+        print('\033[35m-' * 50)
+
+    return inner
+
+
+@info
+def menu_by_category(url, type_parser):
+    create_dir(type_parser)
+
+    site = requests.get(url=url, headers=HEADERS)
+    soup = BeautifulSoup(site.text, 'lxml')
+    for category in soup.find_all('div', class_='col-xl-2 col-md-3 col-6'):
+        url = category.find('a', class_='home-menu-items').get('href')
+        title = url.split('/')[-2]
+        print(f'\033[35m{title}:')
+
+        with open(f'data/{type_parser}/{title}.csv', 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                (
+                    'Название',
+                    'Состав',
+                    'Цена',
+                    'Ссылка на товар'
+                )
+            )
+
+        site_category = requests.get(url=url, headers=HEADERS)
+        soup = BeautifulSoup(site_category.text, 'lxml')
+        try:
+            amount_pages = int(soup.find_all('a', class_='page-numbers')[-2].text)
+        except IndexError:
+            amount_pages = 1
+
+        update_csv_file_new_dishes(url, amount_pages, type_parser, title)
+
+
+@info
+def all_menu(url, type_parser):
+    create_dir(type_parser)
+
+    site = requests.get(url=url, headers=HEADERS)
+    soup = BeautifulSoup(site.text, 'lxml')
+    amount_pages = int(soup.find_all('a', class_='page-numbers')[-2].text)
+
+    with open(f'data/{type_parser}/{type_parser}.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                'Название',
+                'Состав',
+                'Цена',
+                'Ссылка на товар'
+            )
+        )
+
+    update_csv_file_new_dishes(url, amount_pages, type_parser, type_parser)
+
+
+def interface():
+    global lang
+    lang = 'EN'
+    print(f'\033[35m{text2art("MENU   SITE")}')
+    print(f'\033[35m{LANGUAGES[lang]["start_program"]}')
+    change_language = input(f'\033[35m{LANGUAGES[lang]["change_language"]}').strip()
+    if change_language == '+':
+        lang = 'RU' if lang == 'EN' else 'EN'
+        print(f'\033[35m{LANGUAGES[lang]["language_was_change"]} {lang}.')
+    while True:
+        choose_parser = input(f'\033[35m{LANGUAGES[lang]["parser_choice"]}').strip()
+        if choose_parser == '1':
+            url = 'https://sushidom.by/shop/'
+            type_parser = 'all_menu'
+            all_menu(url, type_parser)
+            break
+        elif choose_parser == '2':
+            url = 'https://sushidom.by/'
+            type_parser = 'menu_by_category'
+            menu_by_category(url, type_parser)
+            break
+        else:
+            print(f'\033[35m{LANGUAGES[lang]["unknown_parser"]}')
 
 
 if __name__ == '__main__':
-    main()
+    interface()
